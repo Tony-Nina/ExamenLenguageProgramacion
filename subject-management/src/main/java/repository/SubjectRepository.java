302f279c215cc493079bf809d1124555022ba685
Tony-Nina <lpaqueta627@gmail.com>
2024-09-29 11:08:42 -0500

    subiendo proyecto de lenguaje de programacion

---
+++ subject-management/src/main/java/repository/SubjectRepository.java
@@ -0,0 +1,8 @@
+package repository;
+import model.Subject.java;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubjectRepository extends JpaRepository<Subject, Integer> {
+	
+	
+}
